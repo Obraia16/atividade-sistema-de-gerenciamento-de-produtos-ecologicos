52f21ea079920a45e781aeb06dc2330ef7b475ed
unknown <moserbryan025@gmail.com>
2025-03-27 13:14:36 -0300

    resolvido problema com o gerenciador

--- Bryan-Moser/GerrenciadorProdutos.ts
+++ Bryan-Moser/GerrenciadorProdutos.ts
@@ -1,11 +1,10 @@
 import { ProdutoEcologico } from "./ProdutoEcologico";
 
 export class GerenciadorProdutos {
-    private produtos: ProdutoEcologico[] = [];
+    private produtos: Array<ProdutoEcologico> = new Array;
 
     adicionarProduto(produto: ProdutoEcologico): void {
         this.produtos.push(produto);
-        console.log(`Produto "${produto.nome}" cadastrado com sucesso!`);
     }
 
     listarProdutos(): void {
--- Bryan-Moser/Resultado.ts
+++ Bryan-Moser/Resultado.ts
@@ -1,6 +1,10 @@
 import { AlimentoOrganico } from "./AlimentoOrganico";
-import { ProdutoLimpezaBiodegradavel } from "./ProdutolimpezaBiodegradavel";
+import { GerenciadorProdutos } from "./GerrenciadorProdutos";
 import { ItemDecoracaoSustentavel } from "./itemDecoracaoSustentavel";
+import { ProdutoLimpezaBiodegradavel } from "./ProdutolimpezaBiodegradavel";
+
+const gerenciador = new GerenciadorProdutos();
+
 
 const maca = new AlimentoOrganico("Maçã Orgânica", 5.99, new Date("2024-12-31"));
 const detergente = new ProdutoLimpezaBiodegradavel("Detergente Natural", 12.50, 500);
@@ -9,5 +13,11 @@ const vaso = new ItemDecoracaoSustentavel("Vaso de Vidro", 29.90, "Vidro Recicla
 console.log(maca);
 console.log(detergente);
 console.log(vaso);
+
+gerenciador.adicionarProduto(maca);
+gerenciador.adicionarProduto(detergente);
+gerenciador.adicionarProduto(vaso);
+
+gerenciador.listarProdutos();
 
 
