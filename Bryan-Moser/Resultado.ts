2d4669b0dd6d22544bc027e9376acb0ed0c4ae2d
unknown <moserbryan025@gmail.com>
2025-03-27 10:54:13 -0300

    Trabalho Finalizado

---
+++ Bryan-Moser/Resultado.ts
@@ -0,0 +1,13 @@
+import { AlimentoOrganico } from "./AlimentoOrganico";
+import { ProdutoLimpezaBiodegradavel } from "./ProdutolimpezaBiodegradavel";
+import { ItemDecoracaoSustentavel } from "./itemDecoracaoSustentavel";
+
+const maca = new AlimentoOrganico("Maçã Orgânica", 5.99, new Date("2024-12-31"));
+const detergente = new ProdutoLimpezaBiodegradavel("Detergente Natural", 12.50, 500);
+const vaso = new ItemDecoracaoSustentavel("Vaso de Vidro", 29.90, "Vidro Reciclado");
+
+console.log(maca);
+console.log(detergente);
+console.log(vaso);
+
+
